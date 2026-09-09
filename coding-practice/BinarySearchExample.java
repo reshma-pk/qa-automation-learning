4abde8254ff0444256f0a5a6627609834f778a08
reshma <reshmadamukrishnan@gmail.com>
2026-09-09 14:43:47 +0400

    Find number in a sorted array

---
+++ coding-practice/BinarySearchExample.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class BinarySearchExample {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = {10, 20, 30, 40, 50}; // sorted array
+        int key = 30;
+
+        int low = 0;
+        int high = arr.length - 1;
+        int mid;
+        boolean found = false;
+
+        while (low <= high) {
+            mid = (low + high) / 2;
+
+            if (arr[mid] == key) {
+                System.out.println("Element found at index: " + mid);
+                found = true;
+                break;
+            } else if (key < arr[mid]) {
+                high = mid - 1; // search left
+            } else {
+                low = mid + 1; // search right
+            }
+        }
+
+        if (!found) {
+            System.out.println("Element not found");
+        }
+
+        sc.close();
+    }
+}
