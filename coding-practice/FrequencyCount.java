03d78b98e482655816aee7df7ad96f223bd6f49e
reshma <reshmadamukrishnan@gmail.com>
2026-09-16 18:50:58 +0400

    Frequency Count

---
+++ coding-practice/FrequencyCount.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+import java.util.TreeMap;
+
+public class FrequencyCount {
+
+    public static void main(String[] args) {
+        int[] arr = {10,30,10,20,30,10};
+
+        Map<Integer,Integer> map = new TreeMap<>();
+
+        for(int num:arr) {
+            map.put(num,map.getOrDefault(num,0) + 1);
+        }
+
+        for(int key:map.keySet()) {
+            System.out.println(key + ": " + map.get(key));
+        }
+    }
+    
+}
